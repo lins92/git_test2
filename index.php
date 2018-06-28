57081446c83ca8f5259fe1412330eb2682ff9562
Mohammed Chouat <mohammedchouat10@gmail.com>
2018-06-28 11:23:47 +0200

    modif h1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GIT test</title>
+    <style>
+    h1{
+        position:absolute;
+        top: 45%;
+        left:45%;
+        color:blue;}
+        </style>
+</head>
+<body>
+<h1>Vivement le mechoui ce midi</h1>
+    
+</body>
+</html>
